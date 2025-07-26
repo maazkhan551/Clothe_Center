05c954b7bb5c61b6ac03129b15a24d07d744cec2
maazkhan551 <bakhthafiz1@gmail.com>
2025-07-26 15:05:05 +0500

    Add animation

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ div = document.querySelector('.services')
 fashion = document.querySelector(".fashion_par")
 support = document.querySelector(".suppo_par")
 contact = document.querySelector(".con")
-console.log(services)
 services[1].addEventListener("click",()=>{
     div.style.display = "block"
     fashion.style.display = "none"
@@ -20,3 +19,20 @@ services[2].addEventListener("click",()=>{
      support.style.display = "none"
     contact.style.display = "none"
 })
+fashion_box = document.querySelectorAll(".fashion_box")
+console.log(fashion_box)
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate');
+        }
+        else{
+            entry.target.classList.remove('animate');
+        }
+    });
+},
+);
+
+fashion_box.forEach(box=>{
+        observer.observe(box)
+ });
